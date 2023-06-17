fdb0b68c5aec6f8daf76e73d5b32313eb438de1e
Mico02 <mohamed.hussein122702@gmail.com>
2023-06-16 22:09:34 -0400

    Implemeted a recursive descent parser

--- Parser.cpp
+++ Parser.cpp
@@ -1,10 +1,49 @@
+#pragma once
 #include "Parser.h"
-int16_t valueOfExpr(int8_t tokens[]){
-    
+
+//Value that keeps track of what token is currenltly on
+uint8_t tokenIndex = 0;
+
+int16_t valueOfExpr(int16_t tokens[]){
+    int16_t value = valueOfTerm(tokens);
+    while(tokens[tokenIndex]== PLUS || tokens[tokenIndex] == MINUS){
+        if(tokens[tokenIndex] == PLUS){
+            tokenIndex++;
+            value += valueOfTerm(tokens);
+        } else {
+            tokenIndex++;
+            value -= valueOfTerm(tokens);
+        }
+    }
+    return value;
 }
-int16_t valueOfTerm(int8_t tokens[]){
+
+int16_t valueOfTerm(int16_t tokens[]){
+    int16_t value = valueOfFactor(tokens);
     
+    while(tokens[tokenIndex]== TIMES || tokens[tokenIndex] == DIVIDE){
+        if(tokens[tokenIndex] == TIMES){
+           tokenIndex++;
+           value *= valueOfFactor(tokens);
+        } else {
+           tokenIndex++;
+           value /= valueOfFactor(tokens);
+        }
+    }
+    return value;
 }
-int16_t valueOfFactor(int8_t tokens[]){
-    
+
+int16_t valueOfFactor(int16_t tokens[]){
+    int16_t value = 0;
+    if(tokens[tokenIndex] == LEFTPARENTHESE){
+        tokenIndex++;
+        value = valueOfExpr(tokens);
+        if(tokens[tokenIndex] == RIGHTPARENTHESE){
+            tokenIndex++;
+        }
+    } else if(tokens[tokenIndex] >= 0){
+        value = tokens[tokenIndex];
+        tokenIndex++;
+    }
+    return value;
 }
--- Parser.h
+++ Parser.h
@@ -1,4 +1,25 @@
 #pragma once
-int16_t valueOfExpr(int8_t tokens[]);
-int16_t valueOfTerm(int8_t tokens[]);
-int16_t valueOfFactor(int8_t tokens[]);
+#include <stdint.h>
+//Recursive descent parser tha follows this CFG
+// expr -> term {add-op term}
+// term -> factor { mult-op factor }
+// factor -> ( expr ) | number
+// add-op -> + | -
+// mult-op -> * | /
+// number -> any unsigned number in the range of int16_t
+
+
+//Token values that represent +,-,*,/,(,)
+#define PLUS -111
+#define PLUSIR -111
+#define MINUS -2
+#define TIMES -3
+#define DIVIDE -4
+#define LEFTPARENTHESE -5
+#define RIGHTPARENTHESE -6
+
+//Parser function prototypes
+int16_t valueOfExpr(int16_t tokens[]);
+int16_t valueOfTerm(int16_t tokens[]);
+int16_t valueOfFactor(int16_t tokens[]);
+
